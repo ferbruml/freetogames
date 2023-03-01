adfc8b9036bc2e6799d99e108d41f8dd15959963
Fernanda Brum <fernandabrum@Fernandas-MacBook-Air.local>
2023-02-28 22:26:10 -0300

    chore: creating Actions component

---
+++ src/components/Actions/Actions.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { FaHeart, FaRegHeart, FaInfo } from "react-icons/fa"
+import Modal from "../Modal/Modal"
+import { Game } from "../../interfaces/interfaces"
+interface ActionsProps {
+  setIsGameSelected: (selected: Boolean) => void
+  selected: Boolean
+  game: Game
+}
+
+const Actions = ({ setIsGameSelected, selected, game }: ActionsProps) => {
+  const [open, setOpen] = React.useState<boolean>(false)
+
+  function handleModalOpen() {
+    setOpen(true)
+  }
+
+  /*
+    TODO: make icons more pleasant instead of just painting them white
+  */
+  return (
+    <div className="flex gap-3 absolute top-[20px] right-[20px]">
+      <button
+        onClick={() => {
+          setIsGameSelected(!selected)
+        }}
+      >
+        {selected ? (
+          <FaHeart className="bg-white" />
+        ) : (
+          <FaRegHeart className="bg-white" />
+        )}
+      </button>
+      <button onClick={handleModalOpen}>
+        <FaInfo className="bg-white" />
+      </button>
+      {open ? <Modal open={open} setOpen={setOpen} game={game} /> : null}
+    </div>
+  )
+}
+
+export default Actions
