98913e8749691574a3d256dbfbf108a7ab91e8f1
Fernanda Brum <fernandabrum@Fernandas-MacBook-Air.local>
2023-02-28 22:29:08 -0300

    chore: adding Modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,23 @@
+import * as Dialog from "@radix-ui/react-dialog"
+import GameDetails from "../GameDetails/GameDetails"
+import { ModalProps } from "../../interfaces/interfaces"
+
+const Modal = ({ open, setOpen, game }: ModalProps) => {
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+
+        <Dialog.Content className="fixed p-10 bg-zinc-300 rounded-2xl   top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Dialog.Title className="text-3xl leading-tight font-extrabold text-center">
+            {game.title}
+          </Dialog.Title>
+
+          <GameDetails game={game} />
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
+
+export default Modal
